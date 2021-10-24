f25184c444446d05540a70717f67cc4c0c88f206
Gui37 <guifty32@gmail.com>
2021-10-24 17:44:11 +0200

    Adding arrays

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,7 @@
 alert("Hello PEter");
 let age = 25;
 let year = 2889;
+let nome = "Peter";
 
 console.log(age, year);
 
@@ -13,3 +14,10 @@ const pont = 20;
 console.log(pont);
 
 console.log();
+
+const hello = `Hello there my name is ${nome}, I am ${age} and I am from the future, precisely the year ${year}`;
+
+console.log(hello.split(", "));
+
+// Arrays - they are structures that hold multiple values
+const numbers = new Array(1,2,3,4,5,6,7,8,9);
